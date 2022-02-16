bbafbc62ec828197ca1b38f6f6a74ac01873d47a
jacristi <jacristi0428@gmail.com>
2022-02-16 14:14:41 -0500

    feat: basic forest generation

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,11 +5,23 @@ using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private int width, height;
-    [SerializeField] private float tileSize;
+
+    [Header("Tile Details")]
     [SerializeField] private Tile blankTile;
     [SerializeField] private Tile[] tilePrefabs;
     [SerializeField] private Tilemap groundTileMap;
+
+    [Header("Grid Details")]
+    [SerializeField] private float seed;
+    [SerializeField] private int width, height;
+    [SerializeField] private float tileSize;
+    [SerializeField] private int smoothingMinThreshold;
+
+    [Header("Forest Gen")]
+    [Range(0, 100)]
+    [SerializeField] private int forestFillPercent;
+    [SerializeField] private bool useForestSmoothing;
+
 
     [SerializeField] Transform cam;
 
@@ -20,6 +32,25 @@ public class GridManager : MonoBehaviour
     private void Start()
     {
         GenerateGrid();
+        if (useForestSmoothing)
+            SmoothTiles(tilePrefabs[(int)Tile.TileType.Forest]);
+        RenderMap();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("space"))
+            ReloadGrid();
+    }
+
+    public void ReloadGrid()
+    {
+        groundTileMap.ClearAllTiles();
+        foreach (Transform child in this.transform) {
+            Destroy(child.gameObject);
+        }
+        GenerateGrid();
+        if (useForestSmoothing)
+            SmoothTiles(tilePrefabs[(int)Tile.TileType.Forest]);
         RenderMap();
     }
 
@@ -27,43 +58,89 @@ public class GridManager : MonoBehaviour
         return tilePrefabs[Random.Range(0, tilePrefabs.Length)]; // if it's an array
     }
 
-    void GenerateGrid()
-    {   Debug.Log("Generating Grid");
+    private int GetSurroundingTileCount(int gridX, int gridY, Tile tileToCheck) {
+        int tileCount = 0;
+
+        for (int nX = gridX-1; nX <= gridX + 1; nX++) {
+            for (int nY = gridY-1; nY <= gridY + 1; nY++) {
+                bool isInBounds = (nX >= 0 && nX<width && nY >= 0 && nY < height);
+                bool isNotThisTile = (nX != gridX && nY != gridY);
+                bool isTileToCheck = (tileArray[nX,nY] == tileToCheck);
+                if (isInBounds && isNotThisTile && isTileToCheck) {
+                    if (tileArray[nX,nY] == tileToCheck)
+                        tileCount++;
+                }
+            }
+        }
+        return tileCount;
+    }
+
+    private void SmoothTiles(Tile tileToSet)
+    {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (tileArray[x,y] != blankTile)
+                {
+                    int surroundingTileCount = GetSurroundingTileCount(x, y, tileToSet);
+
+                    if (surroundingTileCount >= smoothingMinThreshold) {
+                        tileArray[x,y] = tileToSet;
+                    }
+                }
+            }
+        }
+    }
+
+    private void GenerateGrid()
+    {
+        // seed = Time.time;
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
         tileArray = new Tile[width, height];
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 Tile tileToSpawn = blankTile;
                 if (!(x == 0 || x == width-1 || y <= 1 || y >= height-2)) {
-                    tileToSpawn = PickRandomTile();
+                    // tileToSpawn = tilePrefabs[(int)Tile.TileType.Grassland];
+                    tileToSpawn = (pseudoRandom.Next(0, 100) < forestFillPercent) ? tilePrefabs[(int)Tile.TileType.Forest] : tilePrefabs[(int)Tile.TileType.Grassland];
                 }
                 tileArray[x,y] = tileToSpawn;
             }
         }
     }
 
-    void GenerateRoads() {
+    private void GenerateForest() {}
+
+    private void GenerateMountains() {}
+
+    private void GenerateWater() {}
+
+    private void GenerateRoads() {
         // Take in a grid /2d array and populate road tile locations
         // Return the updated grid
     }
+
+    private void GeneratePlaces() {
+        // Take in a grid/2d array and populate places (starting village, starting bandit camps, etc.)
+        // Target village should be opposite from starting village
+        // bandit camps should be between start and target villages
+        // places replace tile with grassland
+        // Return new places grid
+    }
+
     void RenderMap()
-    {   Debug.Log("Rendering Grid");
+    {
         for (int x=0; x<tileArray.GetLength(0); x++) {
             for (int y=0; y<tileArray.GetLength(1); y++) {
                 Tile thisTile = tileArray[x, y];
                 var tileObj = Instantiate(thisTile, new Vector3(x+.5f, y+.5f), Quaternion.identity);
                 tileObj.transform.parent = this.transform;
-                tileObj.name = $"{tileObj.tileType} {x},{y}";
+                tileObj.name = $"{tileObj.GetTileTypeVerbose()} {x},{y}";
                 groundTileMap.SetTile(new Vector3Int(x, y, 0), thisTile.ruleTile);
             }
         }
 
         cam.transform.position = new Vector3((float)width * 0.5f, (float)height * 0.5f, -10);
-    }
-
-    void GeneratePlaces() {
-        // Take in a grid/2d array and populate places (starting village, starting bandit camps, etc.)
-        // Return new places grid
     }
 
     private Tile GetTileAtPosition(Vector2 pos)
--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -6,14 +6,49 @@ using UnityEngine.Tilemaps;
 
 public class Tile : MonoBehaviour
 {
-    public string tileType;
+    public string tileTypeVerbose;
     [SerializeField] private SpriteRenderer spr;
 
     [SerializeField] private GameObject highlight;
     // [SerializeField] private GameObject selection;
 
+    public enum TileType {Void, Grassland, Forest, Mountain, Water, Road}
+    public TileType tileType = TileType.Void;
+
     public TileBase ruleTile;
 
+    private void Start() {
+        tileTypeVerbose = GetTileTypeVerbose();
+    }
+
+    public string GetTileTypeVerbose() {
+        var typeString = "__";
+        switch (tileType)
+        {
+            case TileType.Void:
+                typeString = "Void";
+                break;
+            case TileType.Grassland:
+                typeString = "Grassland";
+                break;
+            case TileType.Forest:
+                typeString = "Forest";
+                break;
+            case TileType.Mountain:
+                typeString = "Mountain";
+                break;
+            case TileType.Water:
+                typeString = "Water";
+                break;
+            case TileType.Road:
+                typeString = "Road";
+                break;
+            default:
+                typeString = "__";
+                break;
+        }
+        return typeString;
+    }
 
     private void OnMouseEnter() {
         highlight.SetActive(true);
